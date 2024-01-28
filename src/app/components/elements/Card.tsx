5e3bd3e4f362f265265eeb515846c149415b93a1
shishir610 <shishir.srik@gmail.com>
2024-01-27 17:45:51 -0700

    commit via cmd+enter

--- src/app/components/elements/Card.tsx
+++ src/app/components/elements/Card.tsx
@@ -11,10 +11,10 @@ export default function Card({ heading, subHeading }: Props) {
             href="#"
             className="block flex-1 mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 shadow-xl"
         >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-700 ">
                 {heading}
             </h5>
-            <p className="font-normal text-gray-700">{subHeading}</p>
+            <p className="font-normal text-gray-500">{subHeading}</p>
         </a>
     )
 }
